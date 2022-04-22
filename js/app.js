820a6e7d2b6911b899d41bb51918f1d7cb3b386b
nelicepriatniy <92029297+nelicepriatniy@users.noreply.github.com>
2022-04-22 18:40:43 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,39 @@
+// header scrolling
+
+function offsetPosition(element) {
+    var offsetLeft = 0, offsetTop = 0;
+    do {
+        offsetLeft += element.offsetLeft;
+        offsetTop  += element.offsetTop;
+    } while (element = element.offsetParent);
+    return offsetTop;
+}
+
+let scrollBtn = document.querySelectorAll('.scrollbtn');
+scrollBtn.forEach(el => {
+    let elem = el;
+    el.addEventListener('click', function(){
+        let data = elem.getAttribute('data-b');
+        let block = document.querySelector(data);
+        let offset = offsetPosition(block);
+        window.scrollTo(offset, offset);
+        console.log(offset)
+    })
+});
+
+//questions
+
+let questBtn = document.querySelectorAll('.quest__btn');
+
+questBtn.forEach(element => {
+    let el = element;
+    el.addEventListener('click', function() {
+        el.classList.toggle('active');
+        let qId = el.getAttribute('data-q');
+        let qBlock = document.querySelectorAll(qId);
+        qBlock.forEach(element => {
+            element.classList.toggle('active');
+        });
+        console.log(qBlock);
+    })
+});
